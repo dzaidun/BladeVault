279fe4c8f308991fcaa1723e7a4e40b014bf47fb
denys_zaidun <d.zaidun.17@gmail.com>
2026-02-27 16:48:43 +0200

    feat(domain): add IPasswordHasher and IJwtTokenGenerator interfaces

---
+++ BladeVault.Domain/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,12 @@
+﻿using BladeVault.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BladeVault.Domain.Interfaces
+{
+    public interface IJwtTokenGenerator
+    {
+        string Generate(User user);
+    }
+}
